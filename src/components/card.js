15de904f598c33eef4ca478d0e8bd4cbfac4f6f6
joseph <codemasterhtml@gmail.com>
2018-07-17 17:59:22 +0000

    not sure what i did but it might break if i dont commit

--- src/components/card.js
+++ src/components/card.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 
 import Input from './input';
 class Card extends Component {
+    
+    constructor() {
+        super()
+        
+        this.state = {
+            color: 'blue',
+            
+        }
+    } 
+    
     render(){
         return(
             <div className="card">
-            
+                <h1>{this.state.color}</h1>
                 {Input('Color') }
                 {Input('Plural Noun') }
             </div>
