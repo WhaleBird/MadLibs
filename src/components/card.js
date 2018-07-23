6e8a4768cc527426f45419395a0e3277253e02cd
joseph <codemasterhtml@gmail.com>
2018-07-23 18:01:57 +0000

    functionality almost done

--- src/components/card.js
+++ src/components/card.js
@@ -24,17 +24,23 @@ class Card extends Component {
             adjectiveFour: '',
             nounThree: '',
             celebFour: '',
-            adjectiveFive: ''
+            adjectiveFive: '',
+            contentVisible: false
             
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     } 
+    
     
     handleInputChange(event){
         this.setState ({[event.target.name]: event.target.value})
     }
-    
+    handleFormSubmit(event){
+        this.setState({ contentVisible:!this.state.contentVisible })
+        event.preventDefault(); 
+    }
     render(){
         
         const inputData = [
@@ -61,15 +67,20 @@ class Card extends Component {
                 
             ]
         return(
-            <div className="card">
+            <form onSubmit= {this.handleFormSubmit} className="card">
+            <div className="card__inputs">
             {
                 inputData.map(data => Input( (data), this.handleInputChange   ))
             }
-            <Content data={this.state}/>
-            
-            
-            
             </div>
+            <button type="submit">{!this.state.contentVisible ? 'Generate Madlib': 'Clear form'}</button>
+            {
+                 this.state.contentVisible ? <Content data={this.state}/> : ''
+            }
+            
+            
+            
+            </form>
         );
     }
 }
