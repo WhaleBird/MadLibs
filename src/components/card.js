93003028da503de41a7d3cd54ad609d6f4d4d506
joseph <codemasterhtml@gmail.com>
2018-07-25 17:08:02 +0000

    Made the clear button work

--- src/components/card.js
+++ src/components/card.js
@@ -2,13 +2,7 @@ import React, { Component } from 'react';
 
 import Input from './input';
 import Content from './content';
-
-class Card extends Component {
-    
-    constructor() {
-        super()
-        
-        this.state = {
+const INITIAL_STATE = {
             color: '',
             PluralNoun: '',
             adjectiveOne: '',
@@ -27,7 +21,15 @@ class Card extends Component {
             adjectiveFive: '',
             contentVisible: false
             
-        }
+            }
+            
+class Card extends Component {
+    
+    constructor() {
+        super()
+        
+        this.state = INITIAL_STATE;
+            
         this.handleInputChange = this.handleInputChange.bind(this);
         
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -38,8 +40,17 @@ class Card extends Component {
         this.setState ({[event.target.name]: event.target.value})
     }
     handleFormSubmit(event){
-        this.setState({ contentVisible:!this.state.contentVisible })
-        event.preventDefault(); 
+        event.preventDefault();
+            
+        if(this.state.contentVisible){
+            this.setState(INITIAL_STATE)
+        } else{
+            this.setState({ contentVisible: true })
+        }
+            
+         
+            
+            
     }
     render(){
         
