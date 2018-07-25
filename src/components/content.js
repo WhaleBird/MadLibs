8ca5a144a9b3db3709b39ba4ceaa883a62f49df9
joseph <codemasterhtml@gmail.com>
2018-07-25 17:32:52 +0000

    added the number labels pt one

--- src/components/content.js
+++ src/components/content.js
@@ -8,8 +8,14 @@ export default class Content extends Component {
                 ];
     }
     render() {
-        
         const data = this.props.data;
+        for(key in data) {
+            
+        }
+        this.props.data.map((object,index)=> {
+            console.log(index);
+        });
+        
         return (
             <p>
             
